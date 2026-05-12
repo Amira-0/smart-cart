cbf59dab0425ddfe289562b25cf3c90069f801bb
Amira-0 <66s1938@utas.edu.om>
2026-05-12 22:59:02 +0400

    Update db.php

--- db.php
+++ db.php
@@ -1,3 +1,17 @@
 <?php
-$conn = null;
+
+$host = getenv("DB_HOST");
+$port = getenv("DB_PORT");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$name = getenv("DB_NAME");
+
+$conn = new mysqli($host, $user, $pass, $name, (int)$port);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8");
+
 ?>
